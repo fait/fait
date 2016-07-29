bbadfbabbd039a9a19af4f8d0d43d311aa600361
Matt Brennan <mattyb1000@gmail.com>
2016-07-29 22:36:30 +0100

    add node_modules to path

--- index.mk
+++ index.mk
@@ -1,6 +1,11 @@
 # allow node's module resolution algorithm to be used for includes.
 require = $(eval include $(shell ./resolve.js $(1)))
 
+# add node_modules to $PATH so we don't have to prefix it everywhere. also set
+# the shell because mac os seems to need it for path to work
+export SHELL := /bin/bash
+export PATH := $(shell npm bin):$(PATH)
+
 # disable built-in rules. we probably don't need them and they clutter up e.g.
 # debug output.
 .SUFFIXES:
