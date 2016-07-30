eab2a2077dfd2645b928ddd56eb6470cd6f167c6
Matt Brennan <mattyb1000@gmail.com>
2016-07-30 18:33:38 +0100

    WIP

--- index.mk
+++ index.mk
@@ -1,8 +1,20 @@
+# warn about things to help us catch typos.
+MAKEFLAGS += --warn-undefined-variables --warn-undefined-functions
+
 # where is this folder?
-~current-dir = $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+~current-dir := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+
+$(warning $(~current-dir))
 
 # allow node's module resolution algorithm to be used for includes.
-require = $(eval include $(shell $(~current-dir)resolve.js $(1) $(~current-dir)))
+define require
+$(eval ~include-path = $(shell $(~current-dir)resolve.js $(1) $(~current-dir)))
+$(eval ~prev-dir := $(~current-dir))
+$(eval ~current-dir := $(dir $(~include-path)))
+$(eval $(warning include-path:$(~include-path) prev-dir:$(~prev-dir) current-dir:$(~current-dir)))
+$(eval include $(~include-path))
+$(eval ~current-dir := $(~prev-dir))
+endef
 
 # add node_modules to $PATH so we don't have to prefix it everywhere. also set
 # the shell because mac os seems to need it for path to work
@@ -14,12 +26,9 @@ export PATH := $(shell npm bin):$(PATH)
 .SUFFIXES:
 MAKEFLAGS += --no-builtin-rules
 
-# warn about things to help us catch typos.
-MAKEFLAGS += --warn-undefined-variables --warn-undefined-functions
-
 # secondary expansion is really really useful and doesn't get in people's way
 # if they don't want to use it
 .SECONDEXPANSION:
 
 # load fait's utilities
-$(call require, ./variables.mk)
+$(call require, ./variables)
