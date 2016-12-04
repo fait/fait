67ddb7b6df7b56cda58cf8a73a06cf75d0b38c4e
Matt Brennan <mattyb1000@gmail.com>
2016-12-04 22:44:20 +0000

    update docs

--- bootstrap.mk
+++ bootstrap.mk
@@ -6,11 +6,9 @@
 #   bootstrap
 #
 #  install missing modules
-#
 node_modules/%/index.mk: package.json ; npm install $*
 #
 #  load fait
-#
 -include node_modules/fait/index.mk
 #
 #  ╺══════════╸
--- core/utilities.mk
+++ core/utilities.mk
@@ -1,37 +1,45 @@
-# add a folder to the shell PATH
+#   ╭─╮   ⡀
+#   ┼╴╭─╮ ┐ ┼╴
+#   │ ╭─┤ │ │
+#   ┴ ╰─╰ ┴ ╰╯
+#  ╺══════════╸
+#   utilities.mk
+#   
+#   Functions to *make* your life easier.
+#
+#   Add a folder to the shell PATH.
 define register-bin
 export PATH := $(1):$(PATH)
 endef
-
-# add a module's npm bin to the shell PATH
+#
+#   Add this module's npm bin to the shell PATH.
 define register-npm-bin
 $(eval $(call register-bin, $(~module-dir)node_modules/.bin))
 endef
-
-# filter by match
+#
+#   Filter by match
 define filter-match
 $(foreach v,$(2),$(if $(findstring $(1),$(v)),$(v),))
 endef
-
-# filter out by match
+#
+#   Filter out by match
 define filter-out-match
 $(foreach v,$(2),$(if $(findstring $(1),$(v)),,$(v)))
 endef
-
-# automatically create a target's folder
+#
+#   Automatically create a target's folder
 make-target-dir = @mkdir -p $(@D)
-# backwards compat
+#
+#   Backwards compatibility for 1.2
 mkdir = $(make-target-dir)
-
-# get variables that weren't defined at the start of the make session
+#
+#   Get variables that weren't defined at the start of the make session
 user-defined-variables = $(filter-out $(~built-in-variables), $(.VARIABLES))
-
-# export given variables
+#
+#   Export given variables
 define export-variables
 $(eval export ~dummy $(1))
 endef
-
-
-# provide installed version of current module
+#
+#   Provide installed version of current module
 module-version = $(shell awk -v FS=\" '/"version": ".+"/ { print $$4 }' $(~module-dir)package.json)
-
