8d1ce7c69e42071dcc7cf135b107f9cafbd084df
Matt Brennan <matt@153.io>
2017-05-02 17:05:31 +0100

    Don't create index.mk if it doesn't exist

--- bootstrap.mk
+++ bootstrap.mk
@@ -6,7 +6,7 @@
 #   bootstrap
 #
 #  install missing modules
-node_modules/%/index.mk: package.json ; npm install $* ; touch $@
+node_modules/%/index.mk: package.json ; npm install $* ; touch -c $@
 #
 #  load fait
 -include node_modules/fait/index.mk
